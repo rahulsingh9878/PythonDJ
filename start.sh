a6c95067dc173ea35463ed4acbd3489ccbe601e8
Rahul Singh <32161440+rahulsingh9878@users.noreply.github.com>
2025-11-12 20:31:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # helpful for local dev: use PORT env var if present
-PORT=${PORT:-8000}
-exec gunicorn -k uvicorn.workers.UvicornWorker app:app --bind 0.0.0.0:$PORT
+exec gunicorn -k uvicorn.workers.UvicornWorker app:app --bind 0.0.0.0
